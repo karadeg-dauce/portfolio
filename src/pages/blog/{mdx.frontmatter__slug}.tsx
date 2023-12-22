3e6c908342151122b39bf8c439f3235435a20a5e
karadeg-dauce <karadeg.dauce@gmail.com>
2023-12-22 10:44:06 +0100

    Fix bug image blog

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -5,14 +5,14 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
 
-const BlogPost = () => {
-  // const images = data.mdx.frontmatter.hero_image.childrenImageSharp;
-  // const imageData = images.length > 0 ? images[0].gatsbyImageData : null;
-  // const image = imageData ? getImage(imageData) : null;
+const BlogPost = ({data, children}) => {
+  const images = data.mdx.frontmatter.hero_image.childrenImageSharp;
+  const imageData = images.length > 0 ? images[0].gatsbyImageData : null;
+  const image = imageData ? getImage(imageData) : null;
   
   return (
-    /*<Layout pageTitle={data.mdx.frontmatter.title}>
-      { <p>Posted: {data.mdx.frontmatter.date}</p>
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
       {image && (
         <GatsbyImage
           image={image}
@@ -25,31 +25,30 @@ const BlogPost = () => {
           {data.mdx.frontmatter.hero_image_credit_text}
         </a>
       </p>
-      {children} }
-    </Layout>*/
-    <p>Bonjour</p>
+      {children} 
+    </Layout>
   )
 }
 
-// export const query = graphql`
-// query ($id: String) {
-//   mdx(id: {eq: $id}) {
-//     frontmatter {
-//       title
-//       date(formatString: "MMMM D, YYYY")
-//       hero_image_credit_text
-//       hero_image_credit_link
-//       hero_image_alt
-//       hero_image {
-//         childrenImageSharp {
-//           gatsbyImageData
-//         }
-//       }
-//     }
-//   }
-// }
-// `
+export const query = graphql`
+query ($id: String) {
+  mdx(id: {eq: $id}) {
+    frontmatter {
+      title
+      date(formatString: "MMMM D, YYYY")
+      hero_image_credit_text
+      hero_image_credit_link
+      hero_image_alt
+      hero_image {
+        childrenImageSharp {
+          gatsbyImageData
+        }
+      }
+    }
+  }
+}
+`
 
-export const Head = () => <SEO title="titre" description={undefined} pathname={undefined} children={undefined} />
+export const Head = ({data}) => <SEO title={data.mdx.frontmatter.title} description={undefined} pathname={undefined} children={undefined} />
 
 export default BlogPost
