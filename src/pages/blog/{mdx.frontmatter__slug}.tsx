8626dadd4bcc7434277f714e8468275ec027804a
karadeg-dauce <karadeg.dauce@gmail.com>
2023-12-22 17:35:45 +0100

    Fix bug : clean code

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -8,11 +8,9 @@ import SEO from '../../components/seo'
 const BlogPost = ({data, children}) => {
 
   console.log(data)
-  const images = data.mdx.frontmatter.hero_image.childImageSharp;
-  const imageData = images.gatsbyImageData ? images.gatsbyImageData : null;
-  const image = imageData ? getImage(imageData) : null;
+  const image = getImage(data.mdx.frontmatter.hero_image)
 
-  console.log(images)
+  console.log(image)
   
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
@@ -35,22 +33,22 @@ const BlogPost = ({data, children}) => {
 }
 
 export const query = graphql`
-query ($id: String) {
-  mdx(id: {eq: $id}) {
-    frontmatter {
-      title
-      date(formatString: "MMMM D, YYYY")
-      hero_image_credit_text
-      hero_image_credit_link
-      hero_image_alt
-      hero_image {
-        childImageSharp {
-          gatsbyImageData
+  query($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
         }
       }
     }
   }
-}
 `
 
 export const Head = ({data}) => <SEO title={data.mdx.frontmatter.title} description={undefined} pathname={undefined} children={undefined} />
